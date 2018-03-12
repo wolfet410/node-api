ee594da9dc7aafdac8b84828cd07e68b5f443096
Todd Wolfe <wolfet410@gmail.com>
2018-03-12 08:59:58 -0400

    Working child_process.exec

--- powershell/extsharingguest.ps1
+++ powershell/extsharingguest.ps1
@@ -1,33 +1,41 @@
 # Init
-Param(
-	[Parameter(Mandatory=$True)]
-	[string]$GuestName,
+# Param(
+# 	[Parameter(Mandatory=$True)]
+# 	[string]$GuestName,
 
-	[Parameter(Mandatory=$True)]
-	[string]$GuestEmail
-)
+# 	[Parameter(Mandatory=$True)]
+# 	[string]$GuestEmail
+# )
 
 Set-Location -Path "c:\node-api\powershell\"
 
 . ".\constants.ps1"
 . ".\functionsglobal.ps1"
 
-# Cloud
-$cpu = "adm_twolfe@gkn.onmicrosoft.com"
-$cpp = $PATH + "\gknonmic.txt"
-$credCloud = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $cpu, (Get-Content $cpp | ConvertTo-SecureString)
+Write-Host "so, uhhh, sleeping"
 
-# MSOL Module
-Connect-AzureAD -Credential $credCloud
+Start-Sleep -s 30
 
-$t = "t is not being overwritten"
-$t = Get-AzureADUser -SearchString todd.wolfe@gkn.com | findstr /i "wolfe"
+Write-Host "wakey wakey"
 
-# Get-AzureADUser doesn't appear to be running via the api, but runs fine locally
-# Suspect it's because Connect-AzureAD isn't actually connecting, but it's a real pain 
-# in the ass to figure out how / why
-# Need to find an error checking routine that works (maybe the $_. style I read once)
-# Then I can check for errors on each major command and send an alert if it fails
+Email -Caller "extsharingguest" -Msg "started" -Subject "started" -To "todd.wolfe@gkn.com"
+
+# # Cloud
+# $cpu = "adm_twolfe@gkn.onmicrosoft.com"
+# $cpp = $PATH + "\gknonmic.txt"
+# $credCloud = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $cpu, (Get-Content $cpp | ConvertTo-SecureString)
+
+# # MSOL Module
+# Connect-AzureAD -Credential $credCloud
+
+# $t = "t is not being overwritten"
+# $t = Get-AzureADUser -SearchString todd.wolfe@gkn.com | findstr /i "wolfe"
+
+# # Get-AzureADUser doesn't appear to be running via the api, but runs fine locally
+# # Suspect it's because Connect-AzureAD isn't actually connecting, but it's a real pain 
+# # in the ass to figure out how / why
+# # Need to find an error checking routine that works (maybe the $_. style I read once)
+# # Then I can check for errors on each major command and send an alert if it fails
 
 
-Email -Caller "extsharingguest" -Msg "$t" -Subject "test" -To "todd.wolfe@gkn.com"
+# Email -Caller "extsharingguest" -Msg "$t" -Subject "test" -To "todd.wolfe@gkn.com"
--- routes/api/test.js
+++ routes/api/test.js
@@ -2,23 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-	var spawn = require('child_process').spawn, child;
-
-
-// -NonInteractive -ExecutionPolicy Bypass -Command '& c:\\PowerShell\\ps-api\\extsharingguest.ps1 -GuestName '$guestName' -GuestEmail '$guestEmail''
-
-	child = spawn('powershell.exe', ['-NonInteractive', '-ExecutionPolicy Bypass', '-Command "& c:\\node-api\\powershell\\extsharingguest.ps1 -GuestName \'$guestName\' -GuestEmail \'$guestEmail\'"']);
-	child.stdout.on('data',function(data){
-	    console.log('Powershell Data: ' + data);
+	var exec = require('child_process').exec;
+	console.log('before callback, console screen clears once script begins');
+	var script = 'powershell.exe -NonInteractive -ExecutionPolicy Bypass -Command "& c:\\node-api\\powershell\\extsharingguest.ps1"';
+	exec(script, function() {
+		console.log('inside the default callback, writes to the server console');
+		res.send('sends this back to the browser');
 	});
-	child.stderr.on('data',function(data){
-	    console.log('Powershell Errors: ' + data);
-	});
-	child.on('exit',function(){
-	    console.log('Powershell Script finished');
-	});
-	child.stdin.end(); //end input
-	res.send('test powershell api done');
 });
 
 module.exports = router;

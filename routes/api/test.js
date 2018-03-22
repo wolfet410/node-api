10ad5f9bb1108edec91e3bd4c01790d2e419f6c4
Todd Wolfe <wolfet410@gmail.com>
2018-03-22 11:38:57 -0400

    Promises working within test API

--- routes/api/test.js
+++ routes/api/test.js
@@ -1,23 +1,25 @@
 var Gkn = require('gkn-js'),
 	Express = require('express'),
+	Q =require('q'),
 	Router = Express.Router();
 
 Router.get('/', function(req, res, next) {
-	var testResponse = Gkn.Common.powershell('extsharingguest.ps1', '-guestName="name" -guestEmail="email"');
+	var deferred = Q.defer(),
+		result;
 
+	Gkn.Common.powershell('extsharingguest.ps1', '-guestName="name" -guestEmail="email"')
+		.then(function(response) {
+			result = { success: true, status: 200, caller: 'test.js>get', data: response.data }
+			Gkn.Common.debuglog(result);
+			res.status(200).send(result);
+		})
+		.fail(function(response) {
+			result = { success: false, status: 400, caller: 'test.js>get', data: response.data };
+			Gkn.Common.log(result);
+			res.status(400).send(result);
+		});
 
-
-// **** NEED A PROMISE HERE? ****
-
-
-	if (!testResponse.success) {
-		res.status(400).send(testResponse);
-		return;
-	}
-
-	res.status(200).send(testResponse);
 	return;
-
 });
 
 module.exports = Router;
